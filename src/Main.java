2e261e4f605f27de908c957db25885ea0d592289
abdulhadi128740-oss <abdulhadi128740@gmail.com>
2026-03-24 13:32:22 +0500

    Add report menu option (placeholder)

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,4 @@
 System.out.println("1. Add Person (Student/Staff)"); // thoda change karein
 System.out.println("1.Add Person/Staff/Student");
+System.out.println("5. Generate Report");  // NAYA: Adhoora feature
+System.out.println("6. Exit");
